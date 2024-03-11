69632d36b619fcba6e8e2009bf06b2fab0b1d7f0
Nguyễn Hoàng Huy <skill0sp1@gmail.com>
2024-03-11 14:44:37 +0700

    create ucPasswordTextBox

---
+++ QuanLyTraoDoiHang/UCPasswordTextBox.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace QuanLyTraoDoiHang
+{
+    partial class UCPasswordTextBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UCPasswordTextBox));
+            this.btnShow = new QuanLyTraoDoiHang.RJControls.RButton();
+            this.btnHide = new QuanLyTraoDoiHang.RJControls.RButton();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.BackColor = System.Drawing.SystemColors.Control;
+            this.btnShow.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.btnShow.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnShow.BackgroundImage")));
+            this.btnShow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnShow.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnShow.BorderRadius = 0;
+            this.btnShow.BorderSize = 0;
+            this.btnShow.FlatAppearance.BorderSize = 0;
+            this.btnShow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnShow.ForeColor = System.Drawing.Color.White;
+            this.btnShow.Location = new System.Drawing.Point(124, 74);
+            this.btnShow.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(26, 28);
+            this.btnShow.TabIndex = 20;
+            this.btnShow.TextColor = System.Drawing.Color.White;
+            this.btnShow.UseVisualStyleBackColor = false;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnHide
+            // 
+            this.btnHide.BackColor = System.Drawing.SystemColors.Control;
+            this.btnHide.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.btnHide.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnHide.BackgroundImage")));
+            this.btnHide.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnHide.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnHide.BorderRadius = 0;
+            this.btnHide.BorderSize = 0;
+            this.btnHide.FlatAppearance.BorderSize = 0;
+            this.btnHide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHide.ForeColor = System.Drawing.Color.White;
+            this.btnHide.Location = new System.Drawing.Point(203, 64);
+            this.btnHide.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(26, 28);
+            this.btnHide.TabIndex = 19;
+            this.btnHide.TextColor = System.Drawing.Color.White;
+            this.btnHide.UseVisualStyleBackColor = false;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.SystemColors.Control;
+            this.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPass.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPass.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtPass.Location = new System.Drawing.Point(0, 0);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PlaceholderText = "  Old password";
+            this.txtPass.Size = new System.Drawing.Size(254, 34);
+            this.txtPass.TabIndex = 18;
+            this.txtPass.UseSystemPasswordChar = true;
+            // 
+            // UCPasswordTextBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnHide);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.txtPass);
+            this.Name = "UCPasswordTextBox";
+            this.Size = new System.Drawing.Size(254, 153);
+            this.Load += new System.EventHandler(this.UCPasswordTextBox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private RJControls.RButton btnShow;
+        private RJControls.RButton btnHide;
+        private TextBox txtPass;
+    }
+}
---
+++ QuanLyTraoDoiHang/UCPasswordTextBox.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyTraoDoiHang
+{
+    public partial class UCPasswordTextBox : UserControl
+    {
+        public UCPasswordTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            if (txtPass.UseSystemPasswordChar == true)
+            {
+                txtPass.UseSystemPasswordChar = false;
+                btnShow.BringToFront();
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            if (txtPass.UseSystemPasswordChar == false)
+            {
+                txtPass.UseSystemPasswordChar = true;
+                btnHide.BringToFront();
+            }
+        }
+
+        private void UCPasswordTextBox_Load(object sender, EventArgs e)
+        {
+            txtPass.Width = this.Width;
+            this.Height = txtPass.Height;
+            btnHide.Location = new Point(this.Width - btnHide.Width - 3, (this.Height - btnHide.Height) / 2);
+            btnShow.Location = new Point(this.Width - btnHide.Width - 3, (this.Height - btnHide.Height) / 2);
+
+            btnHide.BringToFront();
+
+        }
+    }
+}
