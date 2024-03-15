2621df239b8b8316e63af13346774ec08b154c23
shannonnonshan <khanhminhdoan04@gmail.com>
2024-03-15 15:47:28 +0700

    My shipment done

--- QuanLyTraoDoiHang/UCCartProduct.Designer.cs
+++ QuanLyTraoDoiHang/UCCartProduct.Designer.cs
@@ -30,20 +30,23 @@
         {
             lblPStatus = new Label();
             lblPPrice = new Label();
-            pictureBox1 = new PictureBox();
             cBCart = new CheckBox();
             rBDelete = new RJControls.RButton();
+            panel1 = new Panel();
+            lblProductName = new Label();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // lblPStatus
             // 
             lblPStatus.AutoSize = true;
-            lblPStatus.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            lblPStatus.Location = new Point(1456, 125);
+            lblPStatus.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            lblPStatus.Location = new Point(1461, 100);
             lblPStatus.Margin = new Padding(2, 0, 2, 0);
             lblPStatus.Name = "lblPStatus";
-            lblPStatus.Size = new Size(128, 54);
+            lblPStatus.Size = new Size(108, 46);
             lblPStatus.TabIndex = 6;
             lblPStatus.Text = "status";
             lblPStatus.Click += lblPStatus_Click;
@@ -51,28 +54,18 @@
             // lblPPrice
             // 
             lblPPrice.AutoSize = true;
-            lblPPrice.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            lblPPrice.Location = new Point(960, 125);
+            lblPPrice.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            lblPPrice.Location = new Point(1125, 100);
             lblPPrice.Margin = new Padding(2, 0, 2, 0);
             lblPPrice.Name = "lblPPrice";
-            lblPPrice.Size = new Size(110, 54);
+            lblPPrice.Size = new Size(94, 46);
             lblPPrice.TabIndex = 5;
             lblPPrice.Text = "price";
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.BackColor = SystemColors.Control;
-            pictureBox1.Location = new Point(149, 45);
-            pictureBox1.Margin = new Padding(2);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(259, 193);
-            pictureBox1.TabIndex = 4;
-            pictureBox1.TabStop = false;
             // 
             // cBCart
             // 
             cBCart.AutoSize = true;
-            cBCart.Location = new Point(33, 125);
+            cBCart.Location = new Point(38, 108);
             cBCart.Name = "cBCart";
             cBCart.Size = new Size(34, 33);
             cBCart.TabIndex = 8;
@@ -87,9 +80,9 @@
             rBDelete.BorderSize = 0;
             rBDelete.FlatAppearance.BorderSize = 0;
             rBDelete.FlatStyle = FlatStyle.Flat;
-            rBDelete.Font = new Font("Segoe UI", 11.1F, FontStyle.Bold, GraphicsUnit.Point);
+            rBDelete.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
             rBDelete.ForeColor = Color.Black;
-            rBDelete.Location = new Point(1811, 103);
+            rBDelete.Location = new Point(1816, 72);
             rBDelete.Name = "rBDelete";
             rBDelete.Size = new Size(197, 100);
             rBDelete.TabIndex = 9;
@@ -97,19 +90,50 @@
             rBDelete.TextColor = Color.Black;
             rBDelete.UseVisualStyleBackColor = false;
             // 
+            // panel1
+            // 
+            panel1.Controls.Add(lblProductName);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(111, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(447, 224);
+            panel1.TabIndex = 19;
+            // 
+            // lblProductName
+            // 
+            lblProductName.AutoSize = true;
+            lblProductName.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            lblProductName.Location = new Point(265, 82);
+            lblProductName.Name = "lblProductName";
+            lblProductName.Size = new Size(123, 46);
+            lblProductName.TabIndex = 11;
+            lblProductName.Text = "(name)";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.Control;
+            pictureBox1.Location = new Point(27, 16);
+            pictureBox1.Margin = new Padding(2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(218, 193);
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            // 
             // UCCartProduct
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(244, 237, 204);
+            Controls.Add(panel1);
             Controls.Add(rBDelete);
             Controls.Add(cBCart);
             Controls.Add(lblPStatus);
             Controls.Add(lblPPrice);
-            Controls.Add(pictureBox1);
             Margin = new Padding(2);
             Name = "UCCartProduct";
-            Size = new Size(2182, 295);
+            Size = new Size(2182, 251);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -118,8 +142,10 @@
         #endregion
         private Label lblPStatus;
         private Label lblPPrice;
-        private PictureBox pictureBox1;
         private CheckBox cBCart;
         private RJControls.RButton rBDelete;
+        private Panel panel1;
+        private Label lblProductName;
+        private PictureBox pictureBox1;
     }
 }
--- QuanLyTraoDoiHang/UCShipment.cs
+++ QuanLyTraoDoiHang/UCShipment.cs
@@ -16,5 +16,15 @@ namespace QuanLyTraoDoiHang
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
