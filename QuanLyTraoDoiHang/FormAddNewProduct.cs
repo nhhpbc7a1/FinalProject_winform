014d25f92fa9c61aca90b4d389aaf037f156104c
Nguyễn Hoàng Huy <skill0sp1@gmail.com>
2024-03-18 22:38:04 +0700

    fix ucpasswordbox

---
+++ QuanLyTraoDoiHang/FormAddNewProduct.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyTraoDoiHang
+{
+    public partial class FormAddNewProduct : Form
+    {
+        public FormAddNewProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangePicture_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Select File";
+            openFileDialog1.InitialDirectory = @"C:\";//--"C:\\";
+            openFileDialog1.Filter = "All files (*.*)|*.*|Text File (*.jpg)|*.jpg";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != "")
+            {
+                picboxProduct.BackgroundImage = Image.FromFile(openFileDialog1.FileName);
+                picboxProduct.Refresh(); 
+                textBox1.Text = System.IO.Directory.GetCurrentDirectory();
+                textBox2.Text = openFileDialog1.FileName;
+            }
+
+        }
+    }
+}
--- QuanLyTraoDoiHang/UCPasswordTextBox.cs
+++ QuanLyTraoDoiHang/UCPasswordTextBox.cs
@@ -46,6 +46,5 @@ namespace QuanLyTraoDoiHang
             this.BringToFront();
         }
 
- 
     }
 }
